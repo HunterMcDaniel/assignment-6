1042f95b0bc75a7173bc60bf0012b8bb16f6da83
“Hunter <hunterlmcdaniel@ufl.edu>
2019-11-30 13:08:08 -0500

    Added javascript

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+var members;
+members= ['Dave Grohl', 'Nate Medel','Pat Smear','Taylor Hawkins','Rami Jaffee','Chris Shiflett']
+
+var tour = {
+  Chicago: July 29 & 30, 2018
+  Seattle: September 1, 2018
+  Edmonton: September 4, 2018
+}
+
+var x = 0
+while (i<5){
+  console.log ('times running through the code: ' + x);
+
+  x++;
+}
